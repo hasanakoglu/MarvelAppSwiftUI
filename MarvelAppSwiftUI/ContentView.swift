8fb42123623bb7cf5213d27cd24690e2ef882821
hasan akoglu <hakoglu@ford.com>
2021-06-20 15:18:07 +0100

    Initial Commit

---
+++ MarvelAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarvelAppSwiftUI
+//
+//  Created by Hasan Akoglu on 20/06/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
